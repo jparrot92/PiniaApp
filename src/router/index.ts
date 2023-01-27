f2642516b6c07487f752fcef1970c3d362074f10
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-27 21:09:21 +0100

    Estructura Module Clientes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import CounterOptionsPage from '@/counter/pages/CounterOptionsPage.vue'
 import CounterSetupPage from '@/counter/pages/CounterSetupPage.vue'
+
+import ClientsLayout from '@/clients/layout/ClientsLayout.vue'
+import ListPage from '@/clients/pages/ListPage.vue';
+import ClientPage from '@/clients/pages/ClientPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +19,16 @@ const router = createRouter({
       path: '/counter-setup',
       name: 'counter-setup',
       component: CounterSetupPage
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      component: ClientsLayout,
+      redirect: { name: 'list' },
+      children: [
+        { path: 'list', name: 'list', component: ListPage },
+        { path: '/clients/:id', name: 'client-id', component: ClientPage },
+      ]
     },
   ]
 })
