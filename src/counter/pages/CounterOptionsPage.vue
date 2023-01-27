1cac4d1294bfb3f6eb64d8742fc70730802dedee
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-27 10:23:06 +0100

    Counter Options Store

--- src/counter/pages/CounterOptionsPage.vue
+++ src/counter/pages/CounterOptionsPage.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCounterOptionsStore } from '@/store/counter-options';
+
+const counterStore = useCounterOptionsStore();
+const { count, squareCount } = storeToRefs( counterStore );
 
 </script>
 
 <template>
     <div>
         <h1>Pinia Options</h1>
+        <h4>Counter: {{ count }}</h4>
+        <h4>Square: {{ squareCount }}</h4>
     </div>
 </template>
 
