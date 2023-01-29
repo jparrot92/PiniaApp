562cd0dd0a25719b301fdc49da918084ae788d2d
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-29 09:23:21 +0100

    Estructura de los componentes que usaremos

---
+++ src/clients/components/ClientList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <ul>
+        <li>
+            <RouterLink :to="{
+                name: 'client-id',
+                params: { id: 1 }
+            }">
+                Client Name
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
